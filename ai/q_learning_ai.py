c3446e1515e3313c3608fa598ac3656a96e87ed5
Matt Frewin <matthew.frewin@outlook.com>
2025-04-06 15:37:31 +0100

    Interim commit for adding obstacles and Q-Learning work on Phase II (latest Q learning updates)

--- ai/q_learning_ai.py
+++ ai/q_learning_ai.py
@@ -1,5 +1,9 @@
 import random
 from collections import defaultdict
+import pickle
+import os
+
+from torchvision.datasets.utils import verify_str_arg
 
 # Learning params..
 ALPHA   = 0.1     # Learning rate
@@ -25,3 +29,22 @@ def update_q(state, action, reward, next_state):
     old_q = Q[state].get(action, 0)
     future_q = max(Q[next_state].values(), default=0)
     Q[state][action] = old_q + ALPHA * (reward + GAMMA * future_q - old_q)
+
+def load_q_table(filename="q_table.pkl"):
+    if os.path.exists(filename):
+        with open(filename, "rb") as f:
+            data = pickle.load(f)
+            return defaultdict(dict, data)
+    else:
+        print(f"Q-table not found. Path: ({filename})")
+        return defaultdict(dict)
+
+Q_Loaded = load_q_table()
+
+def q_learning_move(enemy, player, board):
+    state = get_state(enemy, player)
+    if state in Q_Loaded:
+        best_action = max(Q_Loaded[state], key=Q_Loaded[state].get, default=random.choice(ACTIONS))
+    else:
+        best_action = random.choice(ACTIONS)
+    enemy.move(*best_action, board)
